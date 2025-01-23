9e40f178eb3f88d26e33f2861a7fdce735e39fa9
Justyce Goode <justyce@andrews.edu>
2025-01-23 14:34:04 -0500

    Implemented the player's gun to follow the mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,8 +15,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerWheelRb;
     public float speed = 3.0f;
 
-    public Vector3 mousePosition3;
-    public Vector2 mousePosition2;
+    //public Vector3 mousePosition3;
+    //public Vector2 mousePosition2;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +27,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Get Input for player movement
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         // transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
@@ -35,9 +36,15 @@ public class PlayerController : MonoBehaviour
         playerWheelRb.AddForce(Vector3.forward * speed * verticalInput);
         playerWheelRb.AddForce(Vector3.right * speed * horizontalInput);
 
-        playerGun.transform.position = playerWheelRb.transform.position + new Vector3(0,0.5f,0);
+        playerGun.transform.position = playerWheelRb.transform.position + new Vector3(0,0.5f,0); //Gun follows player
 
-        mousePosition3 = Input.mousePosition;
-        mousePosition2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Code for the gun to lock at the mouse
+        Vector3 mousePositionScreen = Input.mousePosition;
+        mousePositionScreen.z = Camera.main.transform.position.y - 1;
+        Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(mousePositionScreen);
+
+        Vector2 direction = new Vector2(mousePositionWorld.x, mousePositionWorld.z) - new Vector2(playerGun.transform.position.x, playerGun.transform.position.z);
+        float mouseAngle = Vector2.SignedAngle(Vector2.right, direction);
+        playerGun.transform.eulerAngles = new Vector3 (0, -mouseAngle, 90);
     }
 }
