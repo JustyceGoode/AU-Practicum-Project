590e27c16fe5210ea798c3ca0469cd617763192e
Justyce Goode <justyce@andrews.edu>
2025-01-24 12:01:13 -0500

    Created prefabs for bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float boundary = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if(transform.position.z > boundary || transform.position.z < -boundary){
+            Destroy(gameObject);
+        }
+
+    }
+}
