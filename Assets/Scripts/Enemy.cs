bacfce33e0bb20f838f1c0de034ddb76bfe63495
Justyce Goode <justyce@andrews.edu>
2025-01-28 11:13:37 -0500

    Draft code for refactoring player and enemy movement.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     //Get Enemy parts
-    public GameObject enemyWheel;
+    //public GameObject enemyWheel;
     public GameObject enemyGun;
 
     private float speed = 10.0f;
-    private Rigidbody enemyWheelRb;
+    private Rigidbody enemyRb;
     public GameObject player;
 
     public float dist;
@@ -25,51 +25,51 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemyWheelRb = enemyWheel.GetComponent<Rigidbody>();
-        player = GameObject.Find("Player Wheel");
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 followDirection = (player.transform.position - enemyWheel.transform.position).normalized;
+        Vector3 followDirection = (player.transform.position - transform.position).normalized;
         Vector2 lookDirection = new Vector2(player.transform.position.x, player.transform.position.z) - new Vector2(enemyGun.transform.position.x, enemyGun.transform.position.z);
         lookDirection = lookDirection.normalized;
 
-        dist = Vector3.Distance(enemyWheel.transform.position, player.transform.position);
+        dist = Vector3.Distance(transform.position, player.transform.position);
 
         if(dist > 3.25f){
-            enemyWheelRb.AddForce(followDirection * speed);
+            enemyRb.AddForce(followDirection * speed);
         }
         else if(dist < 2.75f){
-            enemyWheelRb.AddForce(-followDirection * speed);
+            enemyRb.AddForce(-followDirection * speed);
         }
 
         //Keep enemy in bounds
-        if(enemyWheel.transform.position.x > xBoundary){
-            enemyWheel.transform.position = new Vector3(xBoundary, enemyWheel.transform.position.y, enemyWheel.transform.position.z);
+        if(transform.position.x > xBoundary){
+            transform.position = new Vector3(xBoundary, transform.position.y, transform.position.z);
         }
-        if(enemyWheel.transform.position.x < -xBoundary){
-            enemyWheel.transform.position = new Vector3(-xBoundary, enemyWheel.transform.position.y, enemyWheel.transform.position.z);
+        if(transform.position.x < -xBoundary){
+            transform.position = new Vector3(-xBoundary, transform.position.y, transform.position.z);
         }
-        if(enemyWheel.transform.position.z > zBoundary){
-            enemyWheel.transform.position = new Vector3(enemyWheel.transform.position.x, enemyWheel.transform.position.y, zBoundary);
+        if(transform.position.z > zBoundary){
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundary);
         }
-        if(enemyWheel.transform.position.z < -zBoundary){
-            enemyWheel.transform.position = new Vector3(enemyWheel.transform.position.x, enemyWheel.transform.position.y, -zBoundary);
+        if(transform.position.z < -zBoundary){
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBoundary);
         }
 
         //enemyWheelRb.AddForce(followDirection * speed);
-        enemyGun.transform.position = enemyWheel.transform.position + new Vector3(0,0.5f,0); //Gun follows enemy
+        enemyGun.transform.position = transform.position + new Vector3(0,0.5f,0); //Gun follows enemy
 
         float lookAngle = Vector2.SignedAngle(Vector2.right, lookDirection);
         enemyGun.transform.eulerAngles = new Vector3 (0, -lookAngle, 90);
 
-        timePassed += Time.deltaTime;
-        if(timePassed > 2f){
-            Instantiate(bulletPrefab, enemyGun.transform.position, enemyGun.transform.rotation);
-            timePassed = 0f;
-        }
+        // timePassed += Time.deltaTime;
+        // if(timePassed > 2f){
+        //     Instantiate(bulletPrefab, enemyGun.transform.position, enemyGun.transform.rotation);
+        //     timePassed = 0f;
+        // }
 
         // if(healthPoints <= 0){
         //     Destroy(gameObject);
