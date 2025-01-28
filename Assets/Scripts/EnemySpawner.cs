ba709d3e42efe227962e5d29830f9aca87cb65d0
Justyce Goode <justyce@andrews.edu>
2025-01-28 14:51:48 -0500

    Bullets can do damage to portals

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,8 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab;
     private float timePassed = 0f;
 
+    private float healthPoints = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,17 @@ public class EnemySpawner : MonoBehaviour
         if(timePassed > 5f){
             Instantiate(enemyPrefab, transform.position + new Vector3(0,1f,0), enemyPrefab.transform.rotation);
             timePassed = 0f;
+        }
+
+        if(healthPoints < 0){
+            Destroy(gameObject);
+        }
+    }
+
+        private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            healthPoints -= 10f;
+            Destroy(other.gameObject);
         }
     }
 }
