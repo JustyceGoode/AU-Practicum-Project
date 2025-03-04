acb4531b234d87765237411e26733a35a08a71c8
Justyce Goode <justyce@andrews.edu>
2025-03-04 15:04:08 -0500

    Organized Title Manager

--- Assets/Scripts/TitleMenuManager.cs
+++ Assets/Scripts/TitleMenuManager.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 
 public class TitleMenuManager : MonoBehaviour
 {
-    //Title Menu Variables
+    //Title Menu Elements
     public TextMeshProUGUI titleText;
     public Button startGameButton;
     public Button controlMenuButton;
     public Button exitGameButton;
 
-    //Control Menu Variables
+    //Control Menu Elements
     public TextMeshProUGUI controlMenuHeaderText;
     public TextMeshProUGUI controlMenuText;
     public Button controlsBackButton;
@@ -21,28 +21,19 @@ public class TitleMenuManager : MonoBehaviour
     public GameObject powerUp;
     public GameObject healthUp;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame(){
         SceneManager.LoadScene("My Game");
         Time.timeScale = 1;
     }
 
     public void OpenControlsMenu(){
+        //Disable Title UI
         titleText.gameObject.SetActive(false);
         startGameButton.gameObject.SetActive(false);
         controlMenuButton.gameObject.SetActive(false);
         exitGameButton.gameObject.SetActive(false);
+
+        //Enable Control UI
         controlMenuHeaderText.gameObject.SetActive(true);
         controlMenuText.gameObject.SetActive(true);
         controlsBackButton.gameObject.SetActive(true);
@@ -52,10 +43,13 @@ public class TitleMenuManager : MonoBehaviour
     }
 
     public void CloseControlsMenu(){
+        //Enable Title UI
         titleText.gameObject.SetActive(true);
         startGameButton.gameObject.SetActive(true);
         controlMenuButton.gameObject.SetActive(true);
         exitGameButton.gameObject.SetActive(true);
+
+        //Disable Contrls UI
         controlMenuHeaderText.gameObject.SetActive(false);
         controlMenuText.gameObject.SetActive(false);
         controlsBackButton.gameObject.SetActive(false);
