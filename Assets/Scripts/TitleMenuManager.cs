e3e65652b14060af7fb4970bcd72bd0749fd2d04
Justyce Goode <justyce@andrews.edu>
2025-03-11 16:27:03 -0400

    Created settings menu in Title Scene

--- Assets/Scripts/TitleMenuManager.cs
+++ Assets/Scripts/TitleMenuManager.cs
@@ -7,13 +7,14 @@ using UnityEngine.UI;
 
 public class TitleMenuManager : MonoBehaviour
 {
-    //Title Menu Elements
+    //Title Menu UI
     public TextMeshProUGUI titleText;
+    public Button settingsButton;
     public Button startGameButton;
     public Button controlMenuButton;
     public Button exitGameButton;
 
-    //Control Menu Elements
+    //Control Menu UI
     public TextMeshProUGUI controlMenuHeaderText;
     public TextMeshProUGUI controlMenuText;
     public Button controlsBackButton;
@@ -21,11 +22,19 @@ public class TitleMenuManager : MonoBehaviour
     public GameObject powerUp;
     public GameObject healthUp;
 
-    //Credits Menu Elements
+    //Credits Menu UI
     public Button creditsButton;
     public TextMeshProUGUI creditsMenuHeaderText;
     public TextMeshProUGUI creditsMenuText;
     public Button creditsBackButton;
+
+    //Settings Menu UI
+    public TextMeshProUGUI settingsText;
+    public TextMeshProUGUI bgmText;
+    public Slider bgmSlider;
+    public TextMeshProUGUI sfxText;
+    public Slider sfxSlider;
+    public Button settingsBackButton;
 
     public void StartGame(){
         SceneManager.LoadScene("My Game");
@@ -39,6 +48,7 @@ public class TitleMenuManager : MonoBehaviour
         controlMenuButton.gameObject.SetActive(false);
         exitGameButton.gameObject.SetActive(false);
         creditsButton.gameObject.SetActive(false);
+        settingsButton.gameObject.SetActive(false);
 
         //Enable Control UI
         controlMenuHeaderText.gameObject.SetActive(true);
@@ -56,6 +66,7 @@ public class TitleMenuManager : MonoBehaviour
         controlMenuButton.gameObject.SetActive(true);
         exitGameButton.gameObject.SetActive(true);
         creditsButton.gameObject.SetActive(true);
+        settingsButton.gameObject.SetActive(true);
 
         //Disable Contrls UI
         controlMenuHeaderText.gameObject.SetActive(false);
@@ -73,6 +84,7 @@ public class TitleMenuManager : MonoBehaviour
         controlMenuButton.gameObject.SetActive(false);
         exitGameButton.gameObject.SetActive(false);
         creditsButton.gameObject.SetActive(false);
+        settingsButton.gameObject.SetActive(false);
 
         //Enable Credits UI
         creditsMenuHeaderText.gameObject.SetActive(true);
@@ -87,11 +99,48 @@ public class TitleMenuManager : MonoBehaviour
         controlMenuButton.gameObject.SetActive(true);
         exitGameButton.gameObject.SetActive(true);
         creditsButton.gameObject.SetActive(true);
+        settingsButton.gameObject.SetActive(true);
 
         //Disable Credits UI
         creditsMenuHeaderText.gameObject.SetActive(false);
         creditsMenuText.gameObject.SetActive(false);
         creditsBackButton.gameObject.SetActive(false);
+    }
+
+    public void OpenSettingsMenu(){
+        //Disable Title UI
+        titleText.gameObject.SetActive(false);
+        startGameButton.gameObject.SetActive(false);
+        controlMenuButton.gameObject.SetActive(false);
+        exitGameButton.gameObject.SetActive(false);
+        creditsButton.gameObject.SetActive(false);
+        settingsButton.gameObject.SetActive(false);
+
+        //Enable Settings UI
+        settingsText.gameObject.SetActive(true);
+        bgmText.gameObject.SetActive(true);
+        bgmSlider.gameObject.SetActive(true);
+        sfxText.gameObject.SetActive(true);
+        sfxSlider.gameObject.SetActive(true);
+        settingsBackButton.gameObject.SetActive(true);
+    }
+
+    public void CloseSettingsMenu(){
+        //Enable Title UI
+        titleText.gameObject.SetActive(true);
+        startGameButton.gameObject.SetActive(true);
+        controlMenuButton.gameObject.SetActive(true);
+        exitGameButton.gameObject.SetActive(true);
+        creditsButton.gameObject.SetActive(true);
+        settingsButton.gameObject.SetActive(true);
+
+        //Disable Settings UI
+        settingsText.gameObject.SetActive(false);
+        bgmText.gameObject.SetActive(false);
+        bgmSlider.gameObject.SetActive(false);
+        sfxText.gameObject.SetActive(false);
+        sfxSlider.gameObject.SetActive(false);
+        settingsBackButton.gameObject.SetActive(false);
     }
 
     public void ExitGame(){
