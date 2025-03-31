48a03fb8416766b44058c8bd87c9bb41e520b7e3
Justyce Goode <justyce@andrews.edu>
2025-03-31 11:27:55 -0400

    Draft code for using NavMeshAgent on Enemy 1

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    public Vector3 mousePosition;
-    public Vector3 mousePositionInitial;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +16,6 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePositionInitial = Input.mousePosition;
-        float temp = mousePositionInitial.y;
-        mousePositionInitial.y = mousePositionInitial.z;
-        mousePositionInitial.z = temp;
-
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePositionInitial);
-        // temp = mousePosition.y;
-        // mousePositionInitial.y = mousePosition.z;
-        // mousePosition.z = temp;
-
-        //mousePosition.y = Camera.main.transform.position.y + Camera.main.nearClipPlane;
-
-        //transform.position = mousePosition;
+        agent.SetDestination(player.transform.position);
     }
 }
